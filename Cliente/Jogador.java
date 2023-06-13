dd4fa51630fd238ecfca5a50b2b4a65395d7817a
azuminha <70153509+azuminha@users.noreply.github.com>
2023-06-13 13:35:04 -0300

    Add files via upload

---
+++ Cliente/Jogador.java
@@ -0,0 +1,13 @@
+import java.awt.event.*;
+
+public class Jogador{
+    int posX, posY;
+    double angulo;
+
+    Jogador(int x, int y, double a){
+        posX = x;
+        posY = y;
+        angulo = a;
+    }
+
+}
---
+++ Servidor/Servidor.java
@@ -0,0 +1,13 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class Servidor{
+    public static void main(String[] args){
+        ServerSocket serverSocket = null;
+        try{
+
+        }
+    }
+}
